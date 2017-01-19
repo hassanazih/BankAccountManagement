9811fce5e0bb6553be1b7381cc2b3342c889d1b5
hassan <hassanazih@gmail.com>
2017-01-19 20:03:42 +0100

    fix compile issue : forgot to add validators

--- account-core/src/main/java/fr/sg/account/service/DefaultAccountService.java
+++ account-core/src/main/java/fr/sg/account/service/DefaultAccountService.java
@@ -1,7 +1,9 @@
 package fr.sg.account.service;
 
 import fr.sg.account.domain.Account;
+import fr.sg.account.exception.AccountException;
 import fr.sg.account.repository.AccountDao;
+import fr.sg.account.repository.DefaultAccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,6 +35,29 @@ public class DefaultAccountService implements AccountService {
         validateAccountNumber(accountNumber);
         validatePositiveAmount(amount);
         dao.deposit(find(accountNumber), amount);
+    }
+
+    private void validateAccountNumber( BigInteger accountNumber) {
+        if (accountNumber == null) {
+            throw new IllegalArgumentException("There is no account identified by " + accountNumber);
+        }
+    }
+
+    private void validatePositiveAmount(BigDecimal amount)
+    {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("The amount should be positive");
+        }
+    }
+
+    private void validateFoundAccounts(List<Account> accounts)
+    {
+        if (accounts == null || accounts.isEmpty()) {
+            throw new AccountException(AccountException.AccountError.NOT_FOUND);
+        }
+        if (accounts.size() > 1) {
+            throw new AccountException(AccountException.AccountError.MANY_ACCOUNTS_WITH_SAME_IDENTIFIER);
+        }
     }
 
 
